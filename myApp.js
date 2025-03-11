f66c9e52c97804412b72c72cdcf82d8fbfb1e736
matt n <mathiasnitz@gmx.de>
2025-03-11 14:12:31 +0000

    sendFileAdded

--- myApp.js
+++ myApp.js
@@ -1,13 +1,13 @@
 let express = require('express');
 let app = express();
 
-
+absolutePath = __dirname + '/views/index.html';
  
 
  console.log("Hello World");
 
  app.get("/",(req, res) => {
-    res.send("Hello Express");
+    res.sendFile(absolutePath);
  })
 
  module.exports = app;
