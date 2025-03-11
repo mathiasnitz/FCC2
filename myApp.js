ea2376ca1600866170ed084ad7cecf67d619ed85
matt n <mathiasnitz@gmx.de>
2025-03-11 13:25:40 +0000

    idk

--- myApp.js
+++ myApp.js
@@ -1,41 +1,7 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
  module.exports = app;
+
+ console.log("Hello World");
