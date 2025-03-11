c6023755e451e58e2c9f2d7aee6ca10f13a7db72
matt n <mathiasnitz@gmx.de>
2025-03-11 14:29:22 +0000

    expressStatic3

--- myApp.js
+++ myApp.js
@@ -1,17 +1,12 @@
 let express = require('express');
 let app = express();
 
-absolutePath = __dirname + '/views/index.html';
- 
-
 console.log("Hello World");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/",(req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
- express.static();
-
- module.exports = app;
+module.exports = app;
