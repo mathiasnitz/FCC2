280b93c87f97dcc0c73ff2ac1fb08d962b783596
matt n <mathiasnitz@gmx.de>
2025-03-12 08:33:39 +0000

    req,res,next for all routes

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ let app = express();
 
 console.log("Hello World");
 
+app.use((req,res,next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
@@ -23,9 +28,6 @@ app.get("/json", (req, res) => {
     res.json({ "message": message });
 });
 
-app.get("/", (req,res,next) => {
-    console.log(req.method + " " + req.path + " - " + req.ip);
-    next();
-})
+
 
 module.exports = app;
