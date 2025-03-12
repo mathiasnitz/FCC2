ac10a7f5433653f98f5c088bab9f45ad4604ad85
matt n <mathiasnitz@gmx.de>
2025-03-12 08:40:16 +0000

    chained middleware

--- myApp.js
+++ myApp.js
@@ -18,6 +18,13 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/now", (req, res, next) =>{
+    req.time = new Date().toString();
+    next();
+}, function(req,res){
+    res.json({"time": req.time})
+});
+
 app.get("/json", (req, res) => {
     let message = "Hello json";
 
